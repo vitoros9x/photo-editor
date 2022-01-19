21bb05b3b0f740101e5a2744b00a139d00fd2b65
vitoros9x <anhkhoa9xhn@gmail.com>
2022-01-19 23:51:37 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,101 @@
 import Slider from './Slider'
+import SidebarItem from './SidebarItem';
 import './App.css';
+import { useState } from 'react';
+
+const DEFAULT_OPTIONS = [
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Contrast',
+    property: 'contrast',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Saturation',
+    property: 'saturate',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Grayscale',
+    property: 'grayscale',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Sepia',
+    property: 'sepia',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Hue Rotate',
+    property: 'hue-rotate',
+    value: 0,
+    range: {
+      min: 0,
+      max: 360
+    },
+    unit: 'deg'
+  },
+  {
+    name: 'Blur',
+    property: 'blur',
+    value: 0,
+    range: {
+      min: 0,
+      max: 20
+    },
+    unit: 'px'
+  }
+]
 
 function App() {
+  const [selectedOptionIndex, setsSelectedOptionIndex] = useState(0)
+  const [options, setOptions] = useState(DEFAULT_OPTIONS)
+  const selectedOption = options[selectedOptionIndex]
+
   return (
     <div className="container">
       <div className="main-image">Image</div>
-      <div className="sidebar">sidebar</div>
+      <div className="sidebar">
+        {options.map((option, index) => {
+          return (
+            <SidebarItem 
+              key={index}
+              name={option.name}
+              active={index === selectedOptionIndex}
+              handleClick={() => setsSelectedOptionIndex(index)}
+            />
+          )
+        })}
+      </div>
       <Slider />
     </div>
   );
